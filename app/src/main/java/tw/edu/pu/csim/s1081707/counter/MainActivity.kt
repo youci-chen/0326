2b6e5db3b323fc42286c1ab782c060dc01e8f578
student <s1081707@gm.pu.edu.tw>
2021-03-19 15:50:17 +0800

    button的Onclick的事件

---
+++ app/src/main/java/tw/edu/pu/csim/s1081707/counter/MainActivity.kt
@@ -0,0 +1,22 @@
+package tw.edu.pu.csim.s1081707.counter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    var counter:Int = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun Add(v: View){
+        if(v.id==R.id.button1) {
+            counter++
+        }else{
+            counter+=2
+        }
+        txv.text = counter.toString()
+    }
+}
