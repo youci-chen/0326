f01e05714a28ec58027cbe5343ea76324521ec98
Yomin <s1081707@gm.pu.edu.tw>
2021-03-26 15:23:22 +0800

    OnClickListener和亂數

--- app/src/main/java/tw/edu/pu/csim/s1081707/counter/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/s1081707/counter/MainActivity.kt
@@ -5,11 +5,22 @@ import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     var counter:Int = 0
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        txv.setOnClickListener(this)
+        button3.setOnClickListener(this)
+        button4.setOnClickListener(this)
+        button5.setOnClickListener(this)
+
+        btnHappy.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                counter = (1..100).random()  //亂數
+                txv.text = counter.toString()
+            }
+        })
     }
     fun Add(v: View){
         //if(v.id==R.id.button1)
@@ -17,6 +28,17 @@ class MainActivity : AppCompatActivity() {
             counter++
         }else{
             counter+=2
+        }
+        txv.text = counter.toString()
+    }
+
+    override fun onClick(v: View?) {
+        if(v==button4 || v==txv){
+            counter++
+        }else if(v==button5){
+            counter+=2
+        }else{
+            counter = 0
         }
         txv.text = counter.toString()
     }
